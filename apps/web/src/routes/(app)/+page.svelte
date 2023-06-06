5eecd683b4351a04569d405e4d9170981226e312
Glitch (glitch-hello-node) <none>
2023-06-06 12:05:44 +0000

    Production Push

--- apps/web/src/routes/(app)/+page.svelte
+++ apps/web/src/routes/(app)/+page.svelte
@@ -20,5 +20,5 @@
 </div>
 <br />
 <div class="text-red-900">
-  Warning! CrowdCards is still in Beta Testing, so stuff will be broken. Like now, as you can see the router is broken.
+  Warning! CrowdCards is still in Beta Testing, so stuff will be broken.
 </div>
