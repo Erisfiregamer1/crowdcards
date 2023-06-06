3e9d5b34736a3c1349e57c2e2c7fd934f66ef109
Glitch (glitch-hello-node) <none>
2023-06-06 23:29:29 +0000

    Production Push

--- apps/web/src/lib/ReloadPrompt.svelte
+++ apps/web/src/lib/ReloadPrompt.svelte
@@ -2,6 +2,10 @@
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/prompt-sw.js', { scope: '/' })
+      
+      navigator.serviceWorker.addEventListener('controllerchange', () => {
+  window.location.reload();
+});
     })
   }
 </script>
