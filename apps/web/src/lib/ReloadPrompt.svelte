5bc4ca13407639b05372ae0732bfe616c7895e16
Glitch (glitch-hello-node) <none>
2023-06-06 23:19:19 +0000

    Production Push

--- apps/web/src/lib/ReloadPrompt.svelte
+++ apps/web/src/lib/ReloadPrompt.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
   if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/prompt-sw.js', { scope: '/' })
-  })
-    
-  navigator.serviceWorker.addEventListener('controllerchange', () => {
-    Swal.fire({
-        title: "Refresh required!",
-        text: "New site content has been loaded! To see it, you'll have to refresh the site.",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonText: "Refresh now",
-        cancelButtonText: "Refresh later",
-      }).then((result) => {
-        if (result.isConfirmed) {
-          window.location.reload();
-        }
-      });
+    let reg = await navigator.serviceWorker.register('/prompt-sw.js', { scope: '/' })
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+  window.location.reload();
 });
-}
+  })
 </script>
