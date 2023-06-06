7baa0c4cc60d599c26e0c6dd574527d4ca05b55e
Glitch (glitch-hello-node) <none>
2023-06-06 23:43:15 +0000

    Production Push

--- apps/web/src/lib/ReloadPrompt.svelte
+++ apps/web/src/lib/ReloadPrompt.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      const registration = await navigator.serviceWorker.register('/prompt-sw.js', { scope: '/' })
-      
-      if (registration.installing) {
-        console.log("Service worker installing");
-      } else if (registration.waiting) {
-        console.log("Service worker installed");
-      } else if (registration.active) {
-        console.log("Service worker active");
-      }
+      navigator.serviceWorker.register('/prompt-sw.js', { scope: '/' })
       
       let isControlled = Boolean(navigator.serviceWorker.controller);
 
